0a973f9989ff5d88fb0822276110557bed72ac96
Mateus Rossi <78707153+MateRossi@users.noreply.github.com>
2023-10-04 19:16:11 -0300

    Primeiro Texto

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Paper, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { ptBR } from '@mui/x-data-grid';
+
+const defaultTheme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: '#000000',
+      },
+      secondary: {
+        main: '#434343',
+      },
+    }
+  },
+  ptBR
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme] = useState(defaultTheme);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={theme}>
+      <Paper>
+        <Typography variant="h2">Colé meu bom.</Typography>
+      </Paper>
+    </ThemeProvider>
   )
 }
 
