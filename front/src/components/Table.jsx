3b82dbaef9ddf43d27e32e48dd83df8917696e62
MateRossi <rossijf2@gmail.com>
2024-10-27 18:28:00 -0300

    adicionado componene sortabletable

--- front/src/components/Table.jsx
+++ front/src/components/Table.jsx
@@ -5,20 +5,20 @@ function Table({ items, columnLabels }) {
     return (
         <div className="table-container">
             <table className="table">
-            <thead>
-                <tr>
-                    {columnLabels.map(label => (
-                        <th key={label}>{label}</th>
-                    ))}
-                </tr>
-            </thead>
+                <thead>
+                    <tr>
+                        {columnLabels.map(label => (
+                            <th key={label}>{label}</th>
+                        ))}
+                    </tr>
+                </thead>
 
-            <tbody>
-                {items.map(item => (
-                    <Row key={item.email} item={item} />
-                ))}
-            </tbody>
-        </table>
+                <tbody>
+                    {items.map(item => (
+                        <Row key={item.id} item={item} />
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
--- front/src/pages/admins/GraduatesPage.jsx
+++ front/src/pages/admins/GraduatesPage.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from 'react';
+import Table from '../../components/Table';
 import '../page.css';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function GraduatesPage() {
+    const [graduates, setGraduates] = useState([]);
+    const axiosPrivate = useAxiosPrivate();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        let isMounted = true;
+        const getGraduates = async () => {
+            try {
+                const response = await axiosPrivate.get(`/users/graduates`);
+                if (isMounted) {
+                    setGraduates(response.data);
+                }
+            } catch (err) {
+                console.error(err);
+                navigate('/', { state: { from: location }, replace: true })
+            }
+        }
+
+        getGraduates();
+
+        return () => isMounted = false;
+    }, [axiosPrivate, location, navigate]);
+
     return (
         <div className="page">
             <h1 className='pageTitle'>Egressos cadastrados</h1>
@@ -8,6 +36,7 @@ function GraduatesPage() {
                 <p className='page-subtitle'>
                     Abaixo estão listados os alunos que se declaram egressos do IF Sudeste MG - Campus Juiz de Fora
                 </p>
+                <Table items={graduates} columnLabels={['Matrícula', 'Nome']}/>
             </main>
         </div>
     );
