921be0c8a5584743a693de8ee943ca8943627847
MateRossi <rossijf2@gmail.com>
2024-12-06 10:26:16 -0300

    ajustada a função de envio de emails para usar variáveis de embiente

--- back/src/utils/mailer.ts
+++ back/src/utils/mailer.ts
@@ -1,5 +1,11 @@
 import nodemailer from 'nodemailer';
 
+const { SYS_MAIL, SYS_PASS, CLIENT_ID, SECRET, REFRESH_TOKEN } = process.env;
+
+if (!SYS_MAIL || !SYS_PASS || !CLIENT_ID || !SECRET || !REFRESH_TOKEN) {
+    throw new Error('Missing required environment variables for sending emails.')
+}
+
 let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,               
@@ -8,9 +14,9 @@ let transporter = nodemailer.createTransport({
         type: 'OAuth2',
         user: SYS_MAIL,
         pass: SYS_PASS,
-        clientId: clientId,
-        clientSecret: secret,
-        refreshToken: refreshToken,
+        clientId: CLIENT_ID,
+        clientSecret: SECRET,
+        refreshToken: REFRESH_TOKEN,
     },
 } as nodemailer.TransportOptions);
 
