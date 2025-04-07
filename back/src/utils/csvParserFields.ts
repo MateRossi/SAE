eea665e9a6a7c45893b829aa0a30136215e481a3
MateRossi <rossijf2@gmail.com>
2025-04-07 12:46:30 -0300

    Download de informações de egressos
    Permite download de arquivos .csv contendo as informações de:
    - Todos os egressos;
    - Egressos que responderam ao acompanhamento de carreira;
    - Egressos que responderam a avaliação institucional;
    - Egressos que responderam a ambos.

--- back/src/utils/csvParserFields.ts
+++ back/src/utils/csvParserFields.ts
@@ -1,24 +1,133 @@
 export const followUpFields = [
-    { label: 'ID', value: 'id' },
-    { label: 'Matrícula', value: 'enrollment' },
-    { label: 'Nome', value: 'name' },
-    { label: 'Email', value: 'email' },
-    { label: 'Permitir Emails', value: 'allowEmails' },
-    { label: 'Telefone', value: 'phoneNumber' },
-    { label: 'Contar Trajetória?', value: 'tellTrajectory' },
-    { label: 'Trabalhou Antes?', value: 'workedBefore' },
-    { label: 'Nível de Formação', value: 'degreeLevel' },
-    { label: 'Comentários', value: 'commentary' },
-    { label: 'Ano de Ingresso', value: 'entryYear' },
-    { label: 'Ano de Conclusão', value: 'graduationYear' },
-    { label: 'Situação', value: 'survey.situation' },
-    { label: 'Cargo atual', value: 'survey.positionName' },
-    { label: 'Tipo do Emprego Atual', value: 'survey.employmentType' },
-    { label: 'Trabalha na Área de Formação?', value: 'survey.worksInArea' },
-    { label: 'Requisito da Formação no Cargo Atual', value: 'survey.positionEducationRequirement' },
-    { label: 'Curso Externo?', value: 'survey.externalCourseName' },
-    { label: 'Relação com o Curso Anterior', value: 'survey.courseRelationLevel' },
-    { label: 'Nome da Empresa Atual', value: 'survey.companyName' },
-    { label: 'Nome do Curso Externo', value: 'course.name' },
-    { label: 'Atualizado em', value: 'updatedAt' },
+  { label: "ID", value: "id" },
+  { label: "Matrícula", value: "enrollment" },
+  { label: "Nome", value: "name" },
+  { label: "Email", value: "email" },
+  { label: "Permitir Emails", value: "allowEmails" },
+  { label: "Telefone", value: "phoneNumber" },
+  { label: "Contar Trajetória?", value: "tellTrajectory" },
+  { label: "Trabalhou Antes?", value: "workedBefore" },
+  { label: "Nível de Formação", value: "degreeLevel" },
+  { label: "Comentários", value: "commentary" },
+  { label: "Ano de Ingresso", value: "entryYear" },
+  { label: "Ano de Conclusão", value: "graduationYear" },
+  { label: "Situação", value: "survey.situation" },
+  { label: "Cargo atual", value: "survey.positionName" },
+  { label: "Tipo do Emprego Atual", value: "survey.employmentType" },
+  { label: "Trabalha na Área de Formação?", value: "survey.worksInArea" },
+  {
+    label: "Requisito da Formação no Cargo Atual",
+    value: "survey.positionEducationRequirement",
+  },
+  { label: "Curso Externo?", value: "survey.externalCourseName" },
+  {
+    label: "Relação com o Curso Anterior",
+    value: "survey.courseRelationLevel",
+  },
+  { label: "Nome da Empresa Atual", value: "survey.companyName" },
+  { label: "Nome do Curso Externo", value: "course.name" },
+  { label: "Atualizado em", value: "updatedAt" },
+];
+
+export const reviewFields = [
+  { label: "ID", value: "id" },
+  { label: "Matrícula", value: "enrollment" },
+  { label: "Nome", value: "name" },
+  { label: "Email", value: "email" },
+  { label: "Permitir Emails", value: "allowEmails" },
+  { label: "Telefone", value: "phoneNumber" },
+  { label: "Contar Trajetória?", value: "tellTrajectory" },
+  { label: "Trabalhou Antes?", value: "workedBefore" },
+  { label: "Nível de Formação", value: "degreeLevel" },
+  { label: "Comentários", value: "commentary" },
+  { label: "Ano de Ingresso", value: "entryYear" },
+  { label: "Ano de Conclusão", value: "graduationYear" },
+
+  // review
+  { label: "Desejo de trabalhar na área", value: "review.desireToWorkArea" },
+  {
+    label: "Avaliação do nível de aprendizado",
+    value: "review.learningLevelRating",
+  },
+  { label: "Avaliação do curso", value: "review.courseRating" },
+  { label: "Avaliação do curso", value: "review.courseRating" },
+  {
+    label: "Avaliação da infraestrutura do campus",
+    value: "review.infraRating",
+  },
+  {
+    label: "Avaliação dos conhecimentos teóricos",
+    value: "review.theoKnowledgeRating",
+  },
+  {
+    label: "Avaliação dos conhecimentos práticos",
+    value: "review.practKnowledgeRating",
+  },
+  { label: "Avaliação dos professores", value: "review.teachersRating" },
+  {
+    label: "Expectativa em relação ao curso",
+    value: "review.courseExpectation",
+  },
+  { label: "Atualizado em", value: "updatedAt" },
+];
+
+export const bothFields = [
+  { label: "ID", value: "id" },
+  { label: "Matrícula", value: "enrollment" },
+  { label: "Nome", value: "name" },
+  { label: "Email", value: "email" },
+  { label: "Permitir Emails", value: "allowEmails" },
+  { label: "Telefone", value: "phoneNumber" },
+  { label: "Contar Trajetória?", value: "tellTrajectory" },
+  { label: "Trabalhou Antes?", value: "workedBefore" },
+  { label: "Nível de Formação", value: "degreeLevel" },
+  { label: "Comentários", value: "commentary" },
+  { label: "Ano de Ingresso", value: "entryYear" },
+  { label: "Ano de Conclusão", value: "graduationYear" },
+
+  //acompanhamento
+  { label: "Situação", value: "survey.situation" },
+  { label: "Cargo atual", value: "survey.positionName" },
+  { label: "Tipo do Emprego Atual", value: "survey.employmentType" },
+  { label: "Trabalha na Área de Formação?", value: "survey.worksInArea" },
+  {
+    label: "Requisito da Formação no Cargo Atual",
+    value: "survey.positionEducationRequirement",
+  },
+  { label: "Curso Externo?", value: "survey.externalCourseName" },
+  {
+    label: "Relação com o Curso Anterior",
+    value: "survey.courseRelationLevel",
+  },
+  { label: "Nome da Empresa Atual", value: "survey.companyName" },
+  { label: "Nome do Curso Externo", value: "course.name" },
+
+  //avaliações
+  { label: "Desejo de trabalhar na área", value: "review.desireToWorkArea" },
+  {
+    label: "Avaliação do nível de aprendizado",
+    value: "review.learningLevelRating",
+  },
+  { label: "Avaliação do curso", value: "review.courseRating" },
+  { label: "Avaliação do curso", value: "review.courseRating" },
+  {
+    label: "Avaliação da infraestrutura do campus",
+    value: "review.infraRating",
+  },
+  {
+    label: "Avaliação dos conhecimentos teóricos",
+    value: "review.theoKnowledgeRating",
+  },
+  {
+    label: "Avaliação dos conhecimentos práticos",
+    value: "review.practKnowledgeRating",
+  },
+  { label: "Avaliação dos professores", value: "review.teachersRating" },
+  {
+    label: "Expectativa em relação ao curso",
+    value: "review.courseExpectation",
+  },
+
+  // atualizado em
+  { label: "Atualizado em", value: "updatedAt" },
 ];
